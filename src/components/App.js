66a3665e9d8490829fa8a92b79630799d31928c9
RumbleXMX2 <zurui.kitsune1337@gmail.com>
2021-11-04 16:24:10 +0900

    refactor app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react'
-import Web3 from 'web3'
-import Navbar from './Navbar'
-import './App.css'
+import React, { Component } from "react";
+import Web3 from "web3";
+import Navbar from "./Navbar";
+import "./App.css";
 
 class App extends Component {
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockChainData()
+    await this.loadWeb3();
+    await this.loadBlockChainData();
   }
 
   async loadBlockChainData() {
-    const web3 = window.web3
+    const web3 = window.web3;
 
-    const accounts = await web3.eth.getAccounts()
-    this.setState({account: accounts[0]})
-    
-    const ethBalance = await web3.eth.getBalance(this.state.account)
-    this.setState({ ethBalance })
-    console.log(this.state.ethBalance)
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+
+    const ethBalance = await web3.eth.getBalance(this.state.account);
+    this.setState({ ethBalance });
+    console.log(this.state.ethBalance);
   }
 
   async loadWeb3() {
-    if(window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
     }
   }
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      account: '',
-      ethBalance: '0'
-    }
+      account: "",
+      ethBalance: "0",
+    };
   }
 
   render() {
     return (
       <div>
-        <Navbar account = {this.state.account}/>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -54,8 +54,8 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
+                  <h1>Hello, World!</h1>
                 </a>
-                <h1>Hello, World!</h1>
               </div>
             </main>
           </div>
